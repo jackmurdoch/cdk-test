a01ab602ee8cdbe66c3354d24401bbcabdc36180
Jack Murdoch <jack.murdoch@nearform.com>
2021-04-29 11:26:31 +0100

    lint fix

--- src/api-lambda/index.ts
+++ src/api-lambda/index.ts
@@ -5,14 +5,18 @@ export const handler: APIGatewayProxyHandler = async (event) => {
   return {
     statusCode: 200,
     headers: { 'Content-Type': 'application/json' },
-    body: JSON.stringify({
-      random: {
-        uuid: faker.datatype.uuid(),
-        firstName: faker.name.firstName(),
-        lastName: faker.name.lastName(),
-        product: faker.commerce.product(),
+    body: JSON.stringify(
+      {
+        random: {
+          uuid: faker.datatype.uuid(),
+          firstName: faker.name.firstName(),
+          lastName: faker.name.lastName(),
+          product: faker.commerce.product(),
+        },
+        event,
       },
-      event
-    }, null, 2),
+      null,
+      2,
+    ),
   };
 };
