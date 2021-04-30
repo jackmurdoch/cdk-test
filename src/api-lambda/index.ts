aaddc8f2af31d493b4587969364c32e8b0e16d60
Jack Murdoch <jack.murdoch@nearform.com>
2021-04-30 16:46:25 +0100

    another attempt

--- src/api-lambda/index.ts
+++ src/api-lambda/index.ts
@@ -1,22 +1,25 @@
 import * as faker from 'faker';
 import { APIGatewayProxyHandler } from 'aws-lambda';
+import { setLambdaHandler } from 'newrelic';
 
-export const handler: APIGatewayProxyHandler = async (event) => {
-  return {
-    statusCode: 200,
-    headers: { 'Content-Type': 'application/json' },
-    body: JSON.stringify(
-      {
-        random: {
-          uuid: faker.datatype.uuid(),
-          firstName: faker.name.firstName(),
-          lastName: faker.name.lastName(),
-          product: faker.commerce.product(),
+export const handler: APIGatewayProxyHandler = setLambdaHandler(
+  async (event) => {
+    return {
+      statusCode: 200,
+      headers: { 'Content-Type': 'application/json' },
+      body: JSON.stringify(
+        {
+          random: {
+            uuid: faker.datatype.uuid(),
+            firstName: faker.name.firstName(),
+            lastName: faker.name.lastName(),
+            product: faker.commerce.product(),
+          },
+          event,
         },
-        event,
-      },
-      null,
-      2,
-    ),
-  };
-};
+        null,
+        2,
+      ),
+    };
+  },
+);
--- src/scheduled-lambda/index.ts
+++ src/scheduled-lambda/index.ts
@@ -1,6 +1,7 @@
 import * as faker from 'faker';
 import { ScheduledHandler } from 'aws-lambda';
+import { setLambdaHandler } from 'newrelic';
 
-export const handler: ScheduledHandler = async () => {
+export const handler: ScheduledHandler = setLambdaHandler(async () => {
   console.log('hello:', faker.name.firstName(), faker.name.lastName());
-};
+});
